bc5c91e440f62a8ae64b2a61a3a365431f2214c2
richie <richiepagard@gmail.com>
2025-10-04 21:43:18 +0330

    feat: Create table in the basic level. Cool :)

---
+++ postgresql/basics/create_table.sql
@@ -0,0 +1,10 @@
+-- Table creation for users --
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	username VARCHAR(50) NOT NULL,
+	email VARCHAR(100),
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Select all rows to see the table
+SELECT * FROM users;
