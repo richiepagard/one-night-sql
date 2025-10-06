d7016ecc6ce55f633f88aa7394f2ec3bca0ea597
richie <richiepagard@gmail.com>
2025-10-07 00:17:29 +0330

    feat: Write a simple fetchig data.
    - Shows how to choose which columns to select and return.
    
    - Explains the `*` kwrod in `SELECT` option.

---
+++ postgresql/basics/fetch_data.sql
@@ -0,0 +1,5 @@
+-- By specifying the column names, we can choose which columns to select --
+SELECT username, email FROM users;
+
+-- Or we can return all columns by using '*' --
+SELECT * FROM users;
